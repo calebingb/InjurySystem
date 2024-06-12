14b5f5be2f774b645cc4422cade1651f8355212c
Caleb F <calebingb@gmail.com>
2024-06-11 18:01:27 -0600

    debugging xplayer

--- server/server.lua
+++ server/server.lua
@@ -1,5 +1,6 @@
 RegisterNetEvent('InjurySystem:playerInjured')
 AddEventHandler('InjurySystem:playerInjured', function (playerID)
+    print (playerID)
     local xPlayer = ESX.GetPlayerFromId(playerID)
 
     print (xPlayer.cid);
