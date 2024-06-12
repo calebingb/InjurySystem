1e5063eac14339ea94359e87f888c543e3ab29f5
Caleb F <calebingb@gmail.com>
2024-06-12 16:50:06 -0600

    Process JSON data

--- client/checkGameInjury.lua
+++ client/checkGameInjury.lua
@@ -4,6 +4,19 @@ RegisterCommand('skel', function (source)
 end, false)
 
 RegisterNetEvent("InjurySystem:retGameInjury")
-AddEventHandler("InjurySystem:retGameInjury", function (json)
-    print(json)
+AddEventHandler("InjurySystem:retGameInjury", function (jsonString)
+    local injuryData, pos, err = json.decode(jsonString, 1, nil)
+    if err then
+        print("Issue with JSON data: " .. error)
+    end
+
+    for boneID, injuries in pairs(injuryData) do
+        print("Injury data for bone " .. boneID .. "...")
+        for _, injury in ipairs(injuries) do
+            local damage = injury.damage
+            local injurySeverity = injury.damageType
+
+            print("Damage: " .. damage .. ", Injury Severity: " .. injurySeverity)
+        end
+    end
 end)
