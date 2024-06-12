98a07fd5747c11d01e2aea97da9bbc1d5442a118
Caleb F <calebingb@gmail.com>
2024-06-12 16:43:10 -0600

    Pulling injuries test

--- server/retGameInjury.lua
+++ server/retGameInjury.lua
@@ -2,6 +2,18 @@ RegisterNetEvent("InjurySystem:getGameInjury")
 AddEventHandler("InjurySystem:getGameInjury", function ()
     local src = source
     local cid = ESX.GetPlayerFromId(src).cid
-    print("Server received request from client with serverID: " .. tostring(src) .. tostring(cid))
-    TriggerClientEvent("InjurySystem:retGameInjury", src, "JSON handshake works!")
+    print("Server received request from client with serverID: " .. tostring(src) .. " and CID: " ..  tostring(cid))
+    TriggerClientEvent("InjurySystem:retGameInjury", src, GetGameInjuries(cid))
 end)
+
+
+function GetGameInjuries (cid)
+    local sql = [[
+    SELECT injuries
+    FROM injuries I
+    WHERE I.cid = ?;]]
+
+    local response = MySQL.prepare.await(sql, {cid})
+    print(json.encode(response, { ident=true, sort_keys = true}))
+    return response
+end
